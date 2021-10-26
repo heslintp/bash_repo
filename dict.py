a4bea399a388551fe3bc791414adf22b9bb662a1
heslintp <heslintp@gmail.com>
2021-10-26 13:23:31 +0300

    new commit

---
+++ dict.py
@@ -0,0 +1,10 @@
+#Dictionaries are used to store data values in key:value pairs
+#
+#A dictionary is a collection which is ordered ,changeable and does not allow duplicates
+
+dict={'Name':'Zara','Age':6,'Class':'First'}
+print(type(dict))
+print(dict)
+# # #
+print("dict[Name]:",dict['Name'])
+print("dict[Age]:",dict['Age'])
